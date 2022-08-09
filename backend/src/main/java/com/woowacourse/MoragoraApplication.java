3e25e7a028db71b1aeadceedbc808249985ea264
progress0407 <progress0407@gmail.com>
2022-08-09 15:36:54 +0900

    update line

--- backend/src/main/java/com/woowacourse/MoragoraApplication.java
+++ backend/src/main/java/com/woowacourse/MoragoraApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MoragoraApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MoragoraApplication.class, args);
     }
 }
